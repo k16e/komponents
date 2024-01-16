1a207eb2f67ad9afd2b566264c162d24ce63a311
kb <kb@k16e.co>
2024-01-16 19:22:01 +0100

    ...

--- src/js/snips.js
+++ src/js/snips.js
@@ -1,5 +1,12 @@
-// Functions
-const slugify = str => {
+// Selectors
+const _body = () => document.body
+const _$ = (selector, parent) => (parent ? parent : document).querySelector(selector)
+const _$$ = (selector, parent) => Array.from((parent ? parent : document).querySelectorAll(selector))
+
+
+
+// Snippets
+const _slugify = str => {
     // Thanks to https://dev.to/bybydev/how-to-slugify-a-string-in-javascript-4o9n
     str = str.replace(/^\s+|\s+$/g, '')
     str = str.toLowerCase()
@@ -11,62 +18,10 @@ const slugify = str => {
 }
 
 
-// Selectors
-const body = () => document.body
-const $$ = (selector, parent) => Array.from((parent ? parent : document).querySelectorAll(selector))
-const $ = (selector, parent) => (parent ? parent : document).querySelector(selector)
-
-
-const slideOut = (obj, enterClasses, leaveClasses) => {
-    obj.classList.remove(...enterClasses)
-    obj.classList.add(...leaveClasses)
-}
-const slideIn = (obj, leaveClasses, enterClasses) => {
-    obj.classList.remove(...leaveClasses)
-    obj.classList.add(...enterClasses)
-}
-
-
-const enter = () => new Array('opacity-100', 'translate-y-0')
-const leave = () => new Array('opacity-0', 'translate-y-12')
-const translateIn = () => new Array('translate-x-0', 'opacity-100')
-const translateOut = () => new Array('translate-x-full', 'opacity-0')
-const hidden = () => new Array('invisible')
-const dimOn = obj => {
-    obj.classList.remove(`opacity-0`)
-    obj.classList.add(`opacity-70`)
-}
-const dimOff = obj => {
-    obj.classList.add(`opacity-0`)
-    obj.classList.remove(`opacity-70`)
-}
-const invisible = obj => obj.classList.add('sr-only')
-const visible = obj => obj.classList.remove('sr-only')
-
-
-const splitFirstOccurrenceOnly = (str, delim) => {
-    // Credit to https://bobbyhadz.com/blog/javascript-split-string-only-on-first-instance-of-character
-    const [first, ...rest] = str.split(delim)
-    const rem = rest.join('')
-
-    return rem
-}
-
-
-const isEmpty = obj => {
-    if (obj === undefined || obj === null) return true
-    if (!Object.keys(obj).length) return true
-}
-
-
-const clearSearch = (input, arr, obj) => {
-    input.value = ''
-    arr.map(a => hidePiece(a))
-}
-
+const _transition = () => new Array('transition', 'duration-300', 'ease-out')
 
 
 // Exports
 export {
-    body, slugify, $, $$, slideOut, slideIn, enter, leave, translateIn, translateOut, hidden, splitFirstOccurrenceOnly, isEmpty, dimOn, dimOff, invisible, visible, clearSearch
+    _body, _slugify, _$, _$$, _transition
 }
