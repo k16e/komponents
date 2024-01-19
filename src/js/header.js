96611c52639fe7cb8f93b6f2d24d33d7039b6b1b
kb <kb@k16e.co>
2024-01-19 09:28:29 +0100

    ditched alpine in favor of nanostores, header actions on scroll

--- src/js/header.js
+++
@@ -1,7 +0,0 @@
-import Alpine from 'alpinejs'
-
-export default function () {
-    const states = Alpine.store('states')
-
-    console.log('Header is', states.scrollPastHeader)
-}
---
+++ src/js/store.js
@@ -0,0 +1,3 @@
+import { atom } from 'nanostores'
+
+export const headerUp = atom(false)
