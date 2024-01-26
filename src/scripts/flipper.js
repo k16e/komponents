f6bebec679f99f1945c161e158cbed9b4d40f32a
kb <kb@k16e.co>
2024-01-26 09:06:47 +0100

    ...

--- src/scripts/flipper.js
+++ src/scripts/flipper.js
@@ -7,27 +7,29 @@ const _flipper = () => {
 
     const
         faces = _ql('[data-flipper="face"]'),
-        backs = _ql('[data-flipper="back"]')
+        backs = _ql('[data-flipper="back"]'),
+        close = ['translate-y-0', 'scale-1', 'opacity-1'],
+        open = ['translate-y-full', 'scale-[0.96]', 'opacity-0']
 
     faces[0].classList.add('translate-y-full')
 
-    faces.forEach(face => {
-        face.addEventListener('click', e => {
+    faces.forEach(i => {
+        i.addEventListener('click', e => {
             const
                 currFace = e.target,
                 otherFaces = faces.filter(i => (i !== currFace))
 
-            if (face.classList.contains('translate-y-0')) face.classList.remove('translate-y-0')
-            face.classList.add('translate-y-full')
-            otherFaces.forEach(i => i.classList.remove('translate-y-full'))
+            if (i.classList.contains('translate-y-0')) i.classList.remove(...close)
+            i.classList.add(...open)
+            otherFaces.forEach(i => i.classList.remove(...open))
         })
     })
-    backs.forEach(back => {
-        back.addEventListener('click', e => {
+    backs.forEach(i => {
+        i.addEventListener('click', e => {
             const backface = e.target.previousElementSibling
 
-            if (backface.classList.contains('translate-y-full')) backface.classList.remove('translate-y-full')
-            backface.classList.add('translate-y-0')
+            if (backface.classList.contains('translate-y-full')) backface.classList.remove(...open)
+            backface.classList.add(...close)
         })
     })
 }
