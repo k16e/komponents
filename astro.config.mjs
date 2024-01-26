b91f7f586f7d1625ab305ea3d6a2192d53ad7ba8
kb <kb@k16e.co>
2024-01-26 08:48:51 +0100

    added started v split

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,6 +30,7 @@ export default defineConfig({
                 splitContent: 'composites/SplitContent',
                 twinSlats: 'composites/TwinSlats',
                 values: 'composites/Values',
+                vSplit: 'composites/VSplit',
             }
         }),
     ],
