e8bf6143b29bd4dc430b99cead654019ebd1f0e5
kb <kb@k16e.co>
2024-01-26 11:58:15 +0100

    ...

--- src/scripts/flipper.js
+++ src/scripts/flipper.js
@@ -9,26 +9,24 @@ const _flipper = () => {
         faces = _ql('[data-flipper="face"]'),
         backs = _ql('[data-flipper="back"]'),
         close = ['translate-y-0', 'scale-1'],
-        open = ['translate-y-full', 'scale-95']
+        open = ['translate-y-full', 'scale-90']
 
     faces[0].classList.add('translate-y-full')
 
-    faces.forEach(i => {
+    faces.map(i => {
         i.addEventListener('click', e => {
-            const
-                currFace = e.target,
-                otherFaces = faces.filter(i => (i !== currFace))
+            const otherFaces = faces.filter(i => (i !== e.target))
 
-            if (i.classList.contains('translate-y-0')) i.classList.remove(...close)
+            i.classList.remove(...close)
             i.classList.add(...open)
-            otherFaces.forEach(i => i.classList.remove(...open))
+            otherFaces.map(i => i.classList.remove(...open))
         })
     })
-    backs.forEach(i => {
+    backs.map(i => {
         i.addEventListener('click', e => {
             const backface = e.target.previousElementSibling
 
-            if (backface.classList.contains('translate-y-full')) backface.classList.remove(...open)
+            backface.classList.remove(...open)
             backface.classList.add(...close)
         })
     })
