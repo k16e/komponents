4cd8e96d3908b7c255c7f07b4abbe44430f0f2fe
kb <kb@k16e.co>
2024-01-27 17:26:04 +0100

    ...

--- src/scripts/slug.js
+++ src/scripts/slug.js
@@ -1,14 +1,14 @@
-const _slug = (linkObj) => {
+const _slug = linkObj => {
     const
         iLink = linkObj.linktype === 'story',
         xLink = linkObj.linktype === 'url'
 
-    if (iLink) {
+    if (iLink && linkObj.story) {
         return linkObj.story.full_slug === 'home' ? '' : linkObj.story.full_slug
+    } else if (!linkObj.story && !xLink) {
+        return linkObj
     }
-    if (xLink) {
-        return linkObj.cached_url
-    }
+    if (xLink) return linkObj.cached_url
 }
 
 export default _slug
