06649624325d5c689579bc98da10dcfbd7850e89
kb <kb@k16e.co>
2024-01-29 16:40:25 +0100

    something that works

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -2,10 +2,12 @@ import { atom } from 'nanostores'
 
 export const
     topbar = atom(false),
-    modal = atom(false)
+    modal = atom(false),
+    _products = atom([])
 
 export function setTopbar() { topbar.set(true) }
 export function setModal() { modal.set(true) }
+export function _addProducts(products) { _products.set(products) }
 
 export function unsetTopbar() { topbar.set(false) }
 export function unsetModal() { modal.set(false) }
