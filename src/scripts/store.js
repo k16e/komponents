ff72417d9ea0729abd8a7556f8e4488379deb09f
kb <kb@k16e.co>
2024-01-30 09:29:40 +0100

    now filtering modal window to only is_new products

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -3,11 +3,13 @@ import { atom } from 'nanostores'
 export const
     topbar = atom(false),
     modal = atom(false),
-    _products = atom([])
+    _products = atom([]),
+    _productsNew = atom([])
 
 export function setTopbar() { topbar.set(true) }
 export function setModal() { modal.set(true) }
 export function _addProducts(products) { _products.set(products) }
+export function _addProductsNew(products) { _productsNew.set(products) }
 
 export function unsetTopbar() { topbar.set(false) }
 export function unsetModal() { modal.set(false) }
