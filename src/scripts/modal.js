56d83ec2f5117a07a5280efdcec25bc0409b4a28
kb <kb@k16e.co>
2024-01-30 09:50:28 +0100

    ...

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -9,8 +9,6 @@ const _modal = () => {
         close = _q('[data-modal-close]'),
         window = _q('[data-modal]'),
         slots = _ql('[data-modal-display]'),
-        inactive = ['sr-only'],
-        active = ['overflow-auto', 'h-full', 'not-sr-only'],
         onn = () => {
             window.classList.remove('translate-x-full', 'opacity-0')
             window.classList.add('translate-x-0', 'opacity-100')
@@ -18,7 +16,7 @@ const _modal = () => {
         off = () => {
             window.classList.remove('translate-x-0', 'opacity-100')
             window.classList.add('translate-x-full', 'opacity-0')
-            slots.map(el => el.classList.add(...inactive))
+            slots.map(el => el.classList.add('sr-only'))
             unsetModal()
         },
         display = (e) => {
@@ -29,12 +27,11 @@ const _modal = () => {
                 attr = target.getAttribute('data-display'),
                 slot = slots.find(el => (el.dataset.modalDisplay === attr))
 
-            slot.classList.remove(...inactive)
-            slot.classList.add(...active)
+            slot.classList.remove('sr-only')
             modal.value ? unsetModal() : setModal()
         }
 
-    slots.map(el => el.classList.add(...inactive))
+    slots.map(el => el.classList.add('sr-only'))
     trigger.map(el => el.addEventListener('click', e => display(e)))
     close.addEventListener('click', () => off())
 
