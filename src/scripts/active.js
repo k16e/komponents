c6880b53c5188bc39f3d37681fcb30111d2c3d8a
kb <kb@k16e.co>
2024-01-31 19:23:52 +0100

    ...

--- src/scripts/active.js
+++ src/scripts/active.js
@@ -1,13 +1,13 @@
 import { _ql, _q } from './snips'
 import _slugify from './slugify'
 
-const _activePath = () => {
+const _active = () => {
     if (!_q('[data-anchor]')) return
 
     const
         navA = _ql('[data-anchor]'),
         currLoc = _slugify(location.pathname),
-        activeA = navA.find(a => (a.getAttribute('data-anchor') === currLoc))
+        activeA = navA.find(a => (a.getAttribute('data-anchor') === currLoc || currLoc.includes(a.getAttribute('data-anchor'))))
 
     if (activeA) {
         navA.map(a => a.classList.remove('active'))
@@ -17,4 +17,4 @@ const _activePath = () => {
     }
 }
 
-export default _activePath
+export default _active
