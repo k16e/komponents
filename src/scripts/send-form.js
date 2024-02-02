067024cb099267c30fa59a8f02051ab081f9b1f0
kb <kb@k16e.co>
2024-02-02 20:13:23 +0100

    form submission test success

--- src/scripts/send-form.js
+++ src/scripts/send-form.js
@@ -15,37 +15,27 @@ const _sendForm = () => {
 
     async function submit(e) {
         e.preventDefault()
+        const
+            form = e.target,
+            data = new FormData(form)
         await fetch(endpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Accept': 'application/json'
             },
-            body: _encode({
-                // pass in the formdata to send
-            })
+            body: _encode(Object.fromEntries(data))
         }).then(res => {
-            // reset form data here
             if (res.status === 200) {
                 console.log('Success')
+                form.reset()
                 // do something if submission success
             }
             else {
                 console.log('Failed')
                 // do something if fails
             }
-        }).catch(err => console.log(err))
-
-        /**
-         * get for data, encode, yeah?
-         * push to localstorage
-         * then load/trigger modal
-         * of course, the form success modal window should be hidden with dummy data
-         * update form modal with data from local storage
-         * on dismiss form modal, clear form (even clear form once submission is successful)
-         * usebasin to test success submission
-         * check how if there's error if show error on same page and clear or show error in modal and reset form
-        */
+        })
     }
 }
 
