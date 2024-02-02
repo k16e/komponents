8746fec5bdef41608310a40c2ac9bdc9e711bea9
kb <kb@k16e.co>
2024-02-02 11:54:21 +0100

    refactored out carousel to own script only run where .swiper present

---
+++ src/scripts/carousel.js
@@ -0,0 +1,31 @@
+import { _q } from './snips'
+import Swiper from 'swiper'
+import { Navigation } from 'swiper/modules'
+
+import 'swiper/css'
+
+const _carousel = () => {
+    if (!_q('.swiper')) return
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        autoplay: { delay: 3500 },
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        grabCursor: true,
+        spaceBetween: 12,
+        autoHeight: true,
+
+        modules: [Navigation],
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        a11y: {
+            prevSlideMessage: 'Previous Slide',
+            nextSlideMessage: 'Next Slide',
+        },
+    })
+}
+
+export default _carousel
