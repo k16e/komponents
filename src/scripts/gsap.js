a854f6d2d7ed8b41ea1c78a2afc308ad26696078
kb <kb@k16e.co>
2024-02-06 14:30:05 +0100

    ...

--- src/scripts/gsap.js
+++ src/scripts/gsap.js
@@ -4,27 +4,27 @@ const _zaps = () => {
     const
         tl = () => gsap.timeline({}),
         slideIn = (props) => ({
-            y: 0,
             opacity: 1,
             visibility: 'visible',
+            y: 0,
             ease: 'back.out',
             ...props
         }),
         slideOut = () => ({
-            y: 48,
             opacity: 0,
             visibility: 0,
+            y: 48,
             ease: 'back.in'
         }),
         slideOff = () => ({
-            y: -48,
             opacity: 0,
             visibility: 0,
-            ease: 'none',
-            duration: .2
+            duration: .2,
+            y: -48,
+            ease: 'none'
         }),
-        slideTop = () => ({ y: 0, opacity: 1, visibility: 'visible', duration: 0.2, ease: 'circ.out' }),
-        slideBottom = () => ({ y: '100%', opacity: 0, visibility: 0, duration: 0.1, ease: 'circ.in' }),
+        slideTop = () => ({ opacity: 1, visibility: 'visible', duration: 0.2, y: 0, ease: 'circ.out' }),
+        slideBottom = () => ({ opacity: 0, visibility: 0, duration: 0.1, y: '100%', ease: 'circ.in' }),
         moveX = props => ({ opacity: 1, visibility: 'visible', duration: 0.3, ease: 'circ.out', ...props })
 
     return { tl, slideIn, slideOut, slideOff, slideTop, slideBottom, moveX }
