ad27a2f75cbce0f17c00f8184329be2ed0c0c784
kb <kb@k16e.co>
2024-02-06 20:31:58 +0100

    autoAlpha really sets opacity and visibility values in one shot

--- src/scripts/zaps.js
+++ src/scripts/zaps.js
@@ -11,10 +11,10 @@ const _zaps = () => {
 
     const
         moveX = (el, props) => tl().to(el, { ...props }),
-        slideOut = (el, props) => tl().to(el, { opacity: 0, visibility: 0, y: 48, ease: 'back.in', ...props }),
+        slideOut = (el, props) => tl().to(el, { autoAlpha: 0, y: 48, ease: 'back.in', ...props }),
         slideIn = (el, props) => tl().to(el, { y: 0, ease: 'back.out', ...props }),
         slideTop = (el, props) => tl().to(el, { y: 0, ...props }),
-        slideBottom = (el, props) => tl().to(el, { opacity: 0, visibility: 0, duration: 0.1, y: '100%', ease: 'circ.in' }),
+        slideBottom = (el, props) => tl().to(el, { autoAlpha: 0, duration: 0.1, y: '100%', ease: 'circ.in' }),
         slideOff = (el, props) => tl().to(el, { y: 0, ease: 'back.out', ...props })
 
     return { moveX, slideOut, slideIn, slideTop, slideBottom, slideOff }
