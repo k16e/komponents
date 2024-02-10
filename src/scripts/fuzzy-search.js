585e4f8309d96be8c34a6b6990b0bde1b7f30165
kb <kb@k16e.co>
2024-02-10 08:39:21 +0100

    finetuning placement of combobox

--- src/scripts/fuzzy-search.js
+++ src/scripts/fuzzy-search.js
@@ -16,45 +16,53 @@ export default function _fuzzySearch() {
         entries = _ql(`[data-list-${wrapper.dataset.list}] > *`),
         typeInterval = 150,
         clear = _q('[data-clear]', wrapper),
-        show = item => {
-            g.on(item)
-            item.style.display = 'block'
-        },
-        hide = item => {
-            g.off(item)
-            item.style.display = 'none'
-        },
-        search = (input, list) => {
-            const
-                query = input.value.toLowerCase().trim(),
-                state = Flip.getState(list)
-
-            list.map(el => {
-                if (el.textContent.toLowerCase().includes(query)) show(el)
-                else hide(el)
-            })
-
-            Flip.from(state, {
-                scale: true,
-                ease: 'power1.inOut',
-                absoluteOnLeave: true,
-                prune: true,
-                absolute: true,
-                fade: true,
-                simple: true,
-                onEnter: els => gsap.fromTo(els, { opacity: 0, scale: 0 }, { opacity: 1, scale: 1 }),
-                onLeave: els => gsap.to(els, { opacity: 0, scale: 0 })
-            })
-        },
-        action = (input, list) => {
-            clearTimeout(typingTimer)
-            typingTimer = setTimeout(search(input, list), typeInterval)
-        }
+        allText = 'All OEMs',
+        firstOpt = _q('[data-first-option="all-oems"]')
 
     clear.addEventListener('click', () => {
+        entries.map(el => show(el))
         input.value = ''
         input.focus()
-        entries.map(el => show(el))
     })
     input.addEventListener('keyup', () => action(input, entries))
+    firstOpt.textContent = allText
+
+
+    function show(item) {
+        g.on(item)
+        item.style.display = 'block'
+    }
+
+    function hide(item) {
+        g.off(item)
+        item.style.display = 'none'
+    }
+
+    function search(input, list) {
+        const
+            query = input.value.toLowerCase().trim(),
+            state = Flip.getState(list)
+
+        list.map(el => {
+            if (el.textContent.toLowerCase().includes(query)) show(el)
+            else hide(el)
+        })
+
+        Flip.from(state, {
+            scale: true,
+            ease: 'power1.inOut',
+            absoluteOnLeave: true,
+            prune: true,
+            absolute: true,
+            fade: true,
+            simple: true,
+            onEnter: els => gsap.fromTo(els, { opacity: 0, scale: 0 }, { opacity: 1, scale: 1 }),
+            onLeave: els => gsap.to(els, { opacity: 0, scale: 0 })
+        })
+    }
+
+    function action(input, list) {
+        clearTimeout(typingTimer)
+        typingTimer = setTimeout(search(input, list), typeInterval)
+    }
 }
