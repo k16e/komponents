9258cc28df2c13cbf5d7db90d9f455899bb2832c
kb <kb@k16e.co>
2024-02-11 11:13:06 +0100

    removed file upload, can justify it

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -12,24 +12,8 @@ export default function _submit() {
     const
         button = _q('[data-form-submit]'),
         endpoint = import.meta.env.PUBLIC_FORM_ENDPOINT,
-        file = _q('[data-input-file]'),
-        fileSuccess = _q('[data-file-success]'),
-        fileError = _q('[data-file-error]'),
         form = button.parentNode,
         successMsg = _q('[data-form-success-message]').textContent
-
-    if (file) {
-        file.addEventListener('change', e => {
-            const
-                files = e.target.files,
-                fileSize = files[0].size,
-                size = Math.round((fileSize / 1024))
-
-            if (files.length) {
-                g.slideTop(fileSuccess, { display: 'flex', y: -6 })
-            }
-        })
-    }
 
     form.addEventListener('submit', submit)
 
