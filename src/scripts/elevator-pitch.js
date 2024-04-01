5d075252fd63771508dab061909866844fb74f9d
kb <kb@k16e.co>
2024-04-01 13:11:45 +0100

    abstracted glide carousel for elevator-pitch into own module, fix error with not loading likely caused by swup on page swap

---
+++ src/scripts/elevator-pitch.js
@@ -0,0 +1,18 @@
+import {
+    _q,
+} from './snips'
+import Glide from '@glidejs/glide'
+
+export default function _elevatorPitch() {
+    if (!_q('[data-elevator-pitch]')) return
+
+    new Glide('[data-elevator-pitch]', {
+        type: 'carousel',
+        autoplay: 3000,
+        perView: 3,
+        breakpoints: {
+            1024: { perView: 2 },
+            576: { perView: 1 },
+        }
+    }).mount()
+}
