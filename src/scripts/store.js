9099d0a68e924b85aaaa908b28439ef14f3f9529
kb <kb@k16e.co>
2024-09-08 06:33:56 +0100

    calling partners in, still as list for now

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -7,15 +7,17 @@ export const
     _products = atom([]),
     _productsNew = atom([]),
     _affiliates = atom([]),
-    _affiliate = persistentAtom('affiliate', '')
+    _affiliate = persistentAtom('affiliate', ''),
+    _partners = atom([])
 
 export function _setOverlay() { _overlay.set(true) }
 export function _setSheet() { _sheet.set(true) }
 
-export function _addProducts(item) { _products.set(item) }
 export function _addProductsNew(item) { _productsNew.set(item) }
 export function _addAffiliates(item) { _affiliates.set(item) }
 export function _addAffiliate(item) { _affiliate.set(item) }
+export function _addProducts(item) { _products.set(item) }
+export function _addPartners(item) { _partners.set(item) }
 
 export function _unsetOverlay() { _overlay.set(false) }
 export function _unsetSheet() { _sheet.set(false) }
