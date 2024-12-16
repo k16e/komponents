0c8a14ebe7fb4f537b27f1b680a52324a259e1a8
K16E, Inc <kb@k16e.co>
2024-12-16 21:13:24 +0100

    jettisoning server island

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,9 +1,10 @@
 const FORM_CONTACT = 'mB9D4ynIB'
 const FORM_REPAIR_CENTER = 'btz2qs5IC'
+const PERPAGE = 10
 const RELATIONS = [
     'feature.entry'
 ]
 
 export {
-    FORM_CONTACT, FORM_REPAIR_CENTER, RELATIONS
+    FORM_CONTACT, FORM_REPAIR_CENTER, RELATIONS, PERPAGE
 }
