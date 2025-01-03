7fff556aa3c6fa37f52cb193617c5e97c03d41ae
K16E, Inc <kb@k16e.co>
2025-01-03 21:57:27 +0100

    finally all working

--- src/scripts/filter-products.js
+++ src/scripts/filter-products.js
@@ -6,11 +6,32 @@ export function _filterProducts() {
     const resetButton = _q('#reset-filters')
     const applyLabel = _q('label[for="apply-filters"]')
     const resetLabel = _q('label[for="reset-filters"]')
+    const searchInput = _q('input[name="s"]')
+
+    let initialState = new Set()
 
     function updateButtonVisibility() {
-        const anyChecked = Array.from(checkboxes).some(cb => cb.checked)
-        applyLabel.style.display = anyChecked ? '' : 'none'
-        resetLabel.style.display = anyChecked ? '' : 'none'
+        const currentState = new Set(Array.from(checkboxes)
+            .filter(cb => cb.checked)
+            .map(cb => cb.value))
+        const stateChanged = !setsAreEqual(initialState, currentState)
+
+        applyLabel.style.display = stateChanged || searchInput.value !== '' ? '' : 'none'
+        resetLabel.style.display = currentState.size > 0 || searchInput.value !== '' ? '' : 'none'
+    }
+
+    function setsAreEqual(a, b) {
+        return a.size === b.size && [...a].every(value => b.has(value))
+    }
+
+    if (window.activePartners && window.activePartners.length > 0) {
+        checkboxes.forEach(cb => {
+            if (window.activePartners.includes(cb.value)) {
+                cb.checked = true
+                initialState.add(cb.value)
+            }
+        })
+        resetLabel.style.display = ''
     }
 
     checkboxes.forEach(checkbox => {
@@ -22,7 +43,10 @@ export function _filterProducts() {
     resetButton.addEventListener('click', e => {
         e.preventDefault()
         checkboxes.forEach(cb => cb.checked = false)
+        searchInput.value = ''
+        initialState.clear()
         updateButtonVisibility()
+        window.location.href = '/products-filtered'
     })
 
     applyButton.addEventListener('click', e => {
@@ -34,6 +58,12 @@ export function _filterProducts() {
         const url = new URL(window.location)
         url.searchParams.delete('partner')
         checkedPartners.forEach(partner => url.searchParams.append('partner', partner))
+
+        if (searchInput.value) {
+            url.searchParams.set('s', searchInput.value)
+        } else {
+            url.searchParams.delete('s')
+        }
 
         window.location.href = url.toString()
     })
